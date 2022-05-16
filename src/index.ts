0169655e85adebf64db903e2bafaa176014b13a1
Ayaka Rizumu <rizumu@oqo.moe>
2022-05-16 22:34:59 +0800

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import { message } from './constants'
+
+export const main = () => {
+  console.log(message)
+}
+
+main()
---
+++ test/basic.test.ts
@@ -0,0 +1,6 @@
+import { expect, test } from 'vitest'
+import { message } from '../src/constants'
+
+test("message contains 'Hello'", () => {
+  expect(message).toContain('Hello')
+})
